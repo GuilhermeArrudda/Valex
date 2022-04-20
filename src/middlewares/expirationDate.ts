31de2a17a4262fa3494c2dd67192b7d1ee752b45
Celso <celsolnv27@gmail.com>
2022-04-19 23:48:08 -0300

    Preparando revisão de código

--- src/middlewares/expirationDate.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-import * as errors from "../utils/errors.js"
-
-export function expirationDate(expirationDate: string) {
-	const today = dayjs().format("MM/YY")
-	if(dayjs(today).isAfter(dayjs(expirationDate))){
-		throw errors.conflict("Card has already expired")
-	}
-}
--- src/schemas/passwordSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-
-const passwordSchema = joi.object({
-	password: joi.string().pattern(/^[0-9]{4}$/).required()
-})
-
-export default passwordSchema
